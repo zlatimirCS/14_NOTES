30c04cb590ce41863426a5ca6a941de85289568a
ZlatimirRamac-CS <zlatimir.ramac@concordsoft.solutions>
2024-11-12 23:49:16 +0100

    03 lesson

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -6,8 +7,12 @@ const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logEvents } = require("./middleware/logger");
 const PORT = process.env.PORT || 3500;
 
+connectDB();
 app.use(logger);
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -30,6 +35,14 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log("ovde je error", error);
+  logEvents(error, "dbErrors.log");
 });
